c6a9b3c3fa1c87bdb01fc16a55cbdc3d87700e51
Ravindar Reddy Ch <Ravindra1507@gmail.com>
2023-03-17 19:17:34 +0530

    Label

--- Login.aspx.cs
+++ Login.aspx.cs
@@ -269,26 +269,10 @@ public partial class Login : System.Web.UI.Page
                         {
                             PlantID = dtBranch.Rows[0]["SuperBranch"].ToString();
                             Session["SuperBranch"] = dtBranch.Rows[0]["SuperBranch"].ToString();
-                            if (PlantID == "172" || Branch == "172" || PlantID == "1801" || Branch == "1801" || PlantID == "3625" || Branch == "3625" || Branch == "3919")
-                            {
-                                Session["TitleName"] = "SRI VYSHNAVI DAIRY SPECIALITIES (P) LTD";
-                                if (Branch == "3625")
-                                {
-                                    Session["Address"] = "No : 170/1B, M.G.R Nagar,Manapakkam,Chennai,TamilNadu (State),PinCode: 600116,TIN No:33140842508,</n>Toll Free No:1800 270 8800 ";
-                                    Session["TinNo"] = "33140842508";
-                                }
-                                else
-                                {
-                                    Session["Address"] = "R.S.No:381/2,Punabaka village Post,Pellakuru Mandal,Nellore District -524129., ANDRAPRADESH (State).Phone: 9440622077, Fax: 044 – 26177799.";
-                                    Session["TinNo"] = "37921042267";
-                                }
-                            }
-                            else
-                            {
-                                Session["TitleName"] = "SRI VYSHNAVI FOODS (P) LTD";
-                                Session["Address"] = " Near Ayyappa Swamy Temple, Shasta Nagar, WYRA-507165,KHAMMAM (District), TELANGANA (State).Phone: 08749 – 251326, Fax: 08749 – 252198.";
-                                Session["TinNo"] = "36550160129";
-                            }
+
+                            Session["TitleName"] = "SRI VYSHNAVI DAIRY SPECIALITIES (P) LTD";
+                            Session["Address"] = "R.S.No:381/2,Punabaka village Post,Pellakuru Mandal,Nellore District -524129., ANDRAPRADESH (State).Phone: 9440622077, Fax: 044 – 26177799.";
+                            Session["TinNo"] = "37921042267";
                             Session["BranchName"] = dtBranch.Rows[0]["BranchName"].ToString();
                         }
                         Session["EmpSno"] = dtemp.Rows[0]["sno"].ToString();
